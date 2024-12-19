602da0c6cb113159bd95d563117196a3d246b2b3
DABronskikh <dima5202@mail.ru>
2024-12-19 21:01:49 +0500

    Спринт 8
    Введение в UI и хранение данных

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,20 +1,32 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btnSearch = findViewById<Button>(R.id.btnSearch)
+        val btnLibrary = findViewById<Button>(R.id.btnLibrary)
+        val btnSettings = findViewById<Button>(R.id.btnSettings)
+
+        btnSearch.setOnClickListener {
+            val displaySearch = Intent(this, SearchActivity::class.java)
+            startActivity(displaySearch)
+        }
+
+        btnLibrary.setOnClickListener {
+            val displayLibrary = Intent(this, MediaLibraryActivity::class.java)
+            startActivity(displayLibrary)
+        }
+
+        btnSettings.setOnClickListener {
+            val displaySettings = Intent(this, SettingsActivity::class.java)
+            startActivity(displaySettings)
         }
     }
 }
